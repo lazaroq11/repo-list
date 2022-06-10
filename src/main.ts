4ecd914a9c1adc6031493d6430373245fdf8d592
Caio <caiobrunopittaf@gmail.com>
2022-06-10 20:37:01 -0300

    template basico vite ts and sass

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import './style.scss';
+
+const app = document.querySelector<HTMLDivElement>('#app')!;
+
+app.innerHTML = `
+  <h1>teste</h1>
+  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+`;
