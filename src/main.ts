c656132157ce9ceb662c21a843bb522ad3e61fc7
lazaroq11 <lazaroq11@hotmail.com>
2022-06-10 22:11:00 -0300

    feat: html and sass components

--- src/main.ts
+++
@@ -1,8 +0,0 @@
-import './style.scss';
-
-const app = document.querySelector<HTMLDivElement>('#app')!;
-
-app.innerHTML = `
-  <h1>teste</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`;
---
+++ src/ts/main.ts
@@ -0,0 +1,7 @@
+import "./sweetAlert.ts";
+import "./apiHandler.ts";
+import "./utils.ts";
+import "./register.ts";
+import "./login.ts";
+import "./statementTable.ts"
+import "./dashboard.ts";
